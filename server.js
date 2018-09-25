f988b980da959866f866555eb7076515af6e03a4
Isaac <theisaacmoses@gmail.com>
2018-09-25 13:35:32 +0530

    Trail 7

--- server.js
+++ server.js
@@ -13,5 +13,13 @@ app.get('/', function(req, res){
 })
 
 
-app.listen(process.env.port || 8080);
+//app.listen(process.env.port || 8080);
 console.log("Listening");
+
+
+
+var server_port = process.env.YOUR_PORT || process.env.PORT || 8080;
+var server_host = process.env.YOUR_HOST || '0.0.0.0';
+app.listen(server_port, server_host, function() {
+    console.log('Listening on port %d', server_port);
+});
