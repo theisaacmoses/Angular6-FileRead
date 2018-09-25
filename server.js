f64d8b5b37f41b8b4d19a91c06b555872d42ebec
Isaac <theisaacmoses@gmail.com>
2018-09-25 13:25:37 +0530

    Trail 5

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ const path = require('path');
 
 app.use(express.static(__dirname + '/dist/hello-world'));
 
-app.listen(process.env.port || 8080);
+
 
 //PathLocationStrategy
 
-app.get('/*', function(req, res){
+app.get('/', function(req, res){
     res.sendFile(path.join(__dirname + '/dist/hello-world/index.html'));
 })
 
+
+app.listen(process.env.port || 8080);
 console.log("Listening");
